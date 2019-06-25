2ae9e7cd54f11fddc20a356cd6b5b13eb5173b40
Werner Wessely <wessely.werner@gmail.com>
2019-06-25 14:51:15 +0200

    Some more parameters behavior

--- src/components/Parameters.vue
+++ src/components/Parameters.vue
@@ -1,17 +1,21 @@
 <template>
     <v-content>
+        <v-container fluid>
         <v-data-table
-            :headers="[]"
-            :items="desserts"
+            :items="table_items"
+            hide-headers
             hide-actions
             disable-initial-sort
         >
             <template v-slot:items="props">
-            <td class="text-xs-left">{{ props.item.name }}</td>
-            <td class="text-xs-left">{{ props.item.calories }}</td>
-            <td class="text-xs-left">{{ props.item.fat }}</td>
+            <td width="200px" class="text-xs-left">{{ props.item.label }}</td>
+            <td width="150px" class="text-xs-left">{{ props.item.current }} {{ props.item.unit }}</td>
+            <td width="200px">
+                <v-slider height="16" v-model="props.item.current" :min="props.item.min" :max="props.item.max" :step="props.item.step"></v-slider>
+            </td>
             </template>
         </v-data-table>
+        </v-container>
     </v-content>
 </template>
 
@@ -19,94 +23,37 @@
   export default {
     data () {
       return {
-        headers: [
-          { value: 'name' },
-          { value: 'value' },
-          { value: 'slider' }
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%'
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%'
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%'
-          }
-        ]
+        parameters: {
+            muzzle_velocity:        {label: 'Muzzle velocity',              init: 75.0,     min: 65.0,  max : 85.0,     unit: 'm/s',    step: 1.0,  current: 0.0},
+            mass:                   {label: 'Mass',                         init: 180.0,    min: 140.0, max : 220.0,    unit: 'g',      step: 1.0,  current: 0.0},
+            initial_attack_angle:   {label: 'Initial attack angle',         init: 8.0,      min: 0.0,   max : 16.0,     unit: 'º',      step: 1.0,  current: 0.0},
+            cross_wind_speed:       {label: 'Cross wind speed',             init: 0.0,      min: 0.0,   max : 10.0,     unit: 'm/s',    step: 1.0,  current: 0.0},
+            trigger_error:          {label: 'Trigger error',                init: 0.01,     min: 0.01,  max : 0.15,     unit: 'º',      step: 0.01, current: 0.0},
+            sighting_error:         {label: 'Sighting error',               init: 0.0,      min: 0.0,   max : 0.07,     unit: 'º',      step: 0.01, current: 0.0},
+            shaking_error:          {label: 'Shaking error',                init: 0.0,      min: 0.0,   max : 0.1,      unit: 'º',      step: 0.01, current: 0.0},
+            elevation_error:        {label: 'Elevation error',              init: 0.0,      min: 0.0,   max : 0.2,      unit: 'º',      step: 0.01, current: 0.0},
+            fuze_error:             {label: 'Fuze error',                   init: 0.0,      min: 0.0,   max : 1.0,      unit: 'ms',     step: 0.1,  current: 0.0},
+            number_of_shots:        {label: 'Number of shots to simulate',  init: 1,        min: 1,     max : 100,      unit: '',       step: 1,    current: 0.0},
+        },
       }
+    },
+
+    computed: {
+        table_items () {
+            return Object.values(this.parameters)
+        }
+    },
+
+    methods: {
+        reset () {
+            this.table_items.forEach( (item) => {
+                item.current = item.init
+            })
+        }
+    },
+
+    mounted () {
+        this.reset()
     }
   }
 </script>
