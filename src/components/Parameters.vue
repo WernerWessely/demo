62afc77071256eb3795a042e0dcf4d7ac308323b
Werner Wessely <wessely.werner@gmail.com>
2019-06-26 16:17:40 +0200

    Reset for parameters

--- src/components/Parameters.vue
+++ src/components/Parameters.vue
@@ -1,20 +1,24 @@
 <template>
     <v-content>
         <v-container fluid>
-        <v-data-table
-            :items="table_items"
-            hide-headers
-            hide-actions
-            disable-initial-sort
-        >
-            <template v-slot:items="props">
-            <td width="200px" class="text-xs-left">{{ props.item.label }}</td>
-            <td width="150px" class="text-xs-left">{{ props.item.current }} {{ props.item.unit }}</td>
-            <td width="200px">
-                <v-slider height="16" v-model="props.item.current" :min="props.item.min" :max="props.item.max" :step="props.item.step"></v-slider>
-            </td>
-            </template>
-        </v-data-table>
+            <v-layout column>
+                <v-flex>
+                    <v-data-table :items="table_items" hide-headers hide-actions disable-initial-sort>
+                        <template v-slot:items="props">
+                        <td width="200px" class="text-xs-left">{{ props.item.label }}</td>
+                        <td width="150px" class="text-xs-left">{{ props.item.current }} {{ props.item.unit }}</td>
+                        <td width="200px">
+                            <v-slider height="16" v-model="props.item.current" :min="props.item.min" :max="props.item.max" :step="props.item.step"></v-slider>
+                        </td>
+                        </template>
+                    </v-data-table>
+                </v-flex>
+                <v-flex>
+                    <div class="text-xs-right">
+                        <v-btn small color="primary" v-on:click="this.reset">Reset</v-btn>
+                    </div>
+                </v-flex>
+            </v-layout>
         </v-container>
     </v-content>
 </template>
