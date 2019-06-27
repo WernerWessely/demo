9fced580d15e68c49eaf6c4865bc687bd174bf83
Werner Wessely <wessely.werner@gmail.com>
2019-06-27 06:10:46 +0200

    Added sim-cancel

--- src/App.vue
+++ src/App.vue
@@ -23,18 +23,9 @@
     </v-content>
 
     <v-toolbar prominent>
-      <v-btn color='primary'>
-      Simulate
-      <v-icon right>play_circle_outline</v-icon>
-      </v-btn>
-       <v-btn color='primary'>
-      Cancel
-      <v-icon right>cancel</v-icon>
-      </v-btn>
+      <v-btn color='primary' v-on:click="running = !running">{{ sim_button.text }}<v-icon right>{{ sim_button.icon }}</v-icon></v-btn>
       <v-spacer></v-spacer>
-      <!--Running
-      <v-progress-circular indeterminate color='primary' size='30'></v-progress-circular>-->
-      
+      <v-progress-circular indeterminate color='primary' size='36' v-if="running"></v-progress-circular>
     </v-toolbar>
 
   </v-app>
@@ -49,22 +40,22 @@ export default {
   components: {
     Parameters
   },
+
   data () {
     return {
-      error_budget: {
-        muzzle_v : {'name': 'Muzzle velocity', 'init': 75, 'min': 65, 'max' : 85, 'unit': 'm/s', 'step': 1},
-        grenade_m : {'name': 'Mass', 'init': 180, 'min': 140, 'max' : 220, 'unit': 'g', 'step': 1},
-        init_attack_angle : {'name': 'Initial attack angle', 'init': 8, 'min': 0, 'max' : 16, 'unit': 'º', 'step': 1},
-        cross_wind_speed : {'name': 'Cross wind speed', 'init': 0, 'min': 0, 'max' : 10, 'unit': 'm/s', 'step': 1},
-        trigger_error : {'name': 'Trigger error', 'init': 0.01, 'min': 0.01, 'max' : 0.15, 'unit': 'º', 'step': 0.01},
-        sighting_error : {'name': 'Sighting error', 'init': 0.0, 'min': 0.0, 'max' : 0.07, 'unit': 'º', 'step': 0.01},
-        shaking_error : {'name': 'Shaking error', 'init': 0.0, 'min': 0.0, 'max' : 0.1, 'unit': 'º', 'step': 0.01},
-        elevation_error : {'name': 'Elevation error', 'init': 0.0, 'min': 0.0, 'max' : 0.2, 'unit': 'º', 'step': 0.01},
-        fuze_error : {'name': 'Fuze error', 'init': 0.0, 'min': 0.0, 'max' : 1.0, 'unit': 'ms', 'step': 0.1}
-      },
-      tabs : 'tab-parameters'
+      tabs: 'tab-parameters',
+      running: false,
     }
-  }
+  },
+
+  computed: {
+    sim_button () {
+      return {
+        text: this.running ? 'Cancel' : 'Simulate',
+        icon: this.running ? 'cancel' : 'play_circle_outline',
+      }
+    }
+  },
 }
 
 </script>
